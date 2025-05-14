8990c501cf965bb7d1b9dc87a6e9713188099bfa
Lukas <lukas.hertel@tum.de>
2025-05-14 19:40:20 +0200

    feat: improve styling of ID page

--- src/tui/pages/id.rs
+++ src/tui/pages/id.rs
@@ -1,5 +1,9 @@
 use qrcode::QrCode;
-use ratatui::widgets::Widget;
+use ratatui::{
+    layout::{Constraint, Flex, Layout, Rect},
+    text::Text,
+    widgets::Widget,
+};
 use tui_qrcode::QrCodeWidget;
 
 pub struct IDPage {
@@ -12,14 +16,36 @@ impl IDPage {
     }
 }
 
+fn center(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect {
+    let [area] = Layout::horizontal([horizontal])
+        .flex(Flex::Center)
+        .areas(area);
+    let [area] = Layout::vertical([vertical]).flex(Flex::Center).areas(area);
+    area
+}
+
 impl Widget for IDPage {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
     {
         // TODO do error handling - e.g., just don't show QR code
-        let qr_code = QrCode::new(self.id).expect("could not generate QR code");
+        let qr_code = QrCode::new(&self.id).expect("could not generate QR code");
         let widget = QrCodeWidget::new(qr_code);
-        widget.render(area, buf);
+
+        let mut qr_area = center(
+            area,
+            Constraint::Length(widget.size(area).width),
+            Constraint::Length(widget.size(area).height),
+        );
+        qr_area.y -= 1;
+
+        let text = Text::raw(self.id);
+        let [mut text_area] = Layout::horizontal([Constraint::Length(text.width() as u16)])
+            .flex(Flex::Center)
+            .areas(area);
+        text_area.y = qr_area.y + qr_area.height;
+        widget.render(qr_area, buf);
+        text.render(text_area, buf);
     }
 }
