77244da63813a01283f3d366f3f5a135f0a3526e
Lukas <lukas.hertel@tum.de>
2025-05-17 12:17:40 +0200

    feat(logging): more flexible logging setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
+use std::path::PathBuf;
+
 use clap::Parser;
-use color_eyre::eyre;
+use color_eyre::eyre::{self, Context};
+use serde::Serialize;
 use syncthing_rs::Client;
 use synctui::{AppConfig, start};
 use tokio::{sync::broadcast, task};
+
+#[derive(clap::ValueEnum, Clone, Debug, Serialize, Default)]
+enum LevelFilter {
+    Off,
+    Error,
+    Warn,
+    Info,
+    #[default]
+    Debug,
+    Trace,
+}
+
+impl From<LevelFilter> for log::LevelFilter {
+    fn from(val: LevelFilter) -> Self {
+        match val {
+            LevelFilter::Off => log::LevelFilter::Off,
+            LevelFilter::Error => log::LevelFilter::Error,
+            LevelFilter::Warn => log::LevelFilter::Warn,
+            LevelFilter::Info => log::LevelFilter::Info,
+            LevelFilter::Debug => log::LevelFilter::Debug,
+            LevelFilter::Trace => log::LevelFilter::Trace,
+        }
+    }
+}
 
 /// CLI wrapper around the syncthing API
 #[derive(Parser, Debug)]
@@ -19,28 +46,67 @@ struct Args {
     /// Provide custom config path
     #[arg(short, long)]
     config: Option<String>,
+
+    /// Set log level
+    #[arg(short, long)]
+    log_level: Option<LevelFilter>,
+
+    /// Set path of log file
+    #[arg(long, requires = "log_level")]
+    log_file: Option<PathBuf>,
 }
 
-fn setup_logging() {
-    let target = Box::new(std::fs::File::create("log.txt").expect("Can't create file"));
+fn default_log_file_path() -> Option<PathBuf> {
+    dirs::cache_dir().map(|mut path| {
+        path.push("synctui");
+        path.push("log.txt");
+        path
+    })
+}
+
+fn setup_logging(path: PathBuf, level: log::LevelFilter) -> eyre::Result<()> {
+    if let Some(parent_dir) = path.parent() {
+        if !parent_dir.as_os_str().is_empty() {
+            std::fs::create_dir_all(parent_dir).wrap_err_with(|| {
+                format!(
+                    "Failed to create parent directory '{}' while preparing log file",
+                    parent_dir.display()
+                )
+            })?;
+        }
+    }
+
+    let target_file = std::fs::File::create(&path)
+        .wrap_err_with(|| format!("Failed to create log file at '{}'", path.display()))?;
+    let target = Box::new(target_file);
 
     env_logger::Builder::new()
         .target(env_logger::Target::Pipe(target))
-        .filter(None, log::LevelFilter::Debug)
+        .filter(None, level)
         .init();
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
-    setup_logging();
     let args = Args::parse();
+    let level = args.log_level;
+    if let Some(level) = level {
+        let path = args.log_file
+            .or_else(default_log_file_path)
+            .ok_or_else(|| eyre::eyre!("Failed to determine a log file path: No path specified via --log-file and could not determine a default path."))?;
+
+        setup_logging(path, level.into())?;
+    }
     let api_key = {
         match args.api_key {
             Some(key) => key,
             None => AppConfig::load(args.config)?.api_key,
         }
     };
+
     let client = Client::builder(&api_key).build()?;
 
     if args.cli {
